ad099916d5fbc4a4fa3bd2376e31095baf4a52e4
VLGorskij <45288874+VLGorskij@users.noreply.github.com>
2020-06-01 13:16:09 +0300

    update pressure value to display in mmHg

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -90,7 +90,7 @@ var checkStates = {
         "lc": 1509870303590
     },
     "mihome.0.devices.weather_v1_1652761251244.pressure": {
-        "val": 751,
+        "val": 751,     // For pressure in mmHg
         "ack": true,
         "ts": 1509870303593,
         "q": 0,
